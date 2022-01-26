b82854b2e2be455306ad9b2ec463e77690d841e3
devmarcstorm <43650740+devmarcstorm@users.noreply.github.com>
2022-01-26 21:28:24 +0100

    Create methods to get a dict value or None if something goes wrong

--- rtp_backend/apps/utilities/user_created_data.py
+++ rtp_backend/apps/utilities/user_created_data.py
@@ -4,6 +4,12 @@ from flask import make_response, request
 from . import http_status_codes as status
 
 
+def get_data_value_or_none(data, key):
+    if data and isinstance(data, dict) and key in data:
+        return data.get(key)
+    return None
+
+
 def make_dict_safe(dict_to_clean: dict) -> dict:
     if dict_to_clean == None:
         return -1
