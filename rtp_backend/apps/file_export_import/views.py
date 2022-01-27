0657d3f05e8ae5c9b288eabab0807ef455f03fcc
devmarcstorm <43650740+devmarcstorm@users.noreply.github.com>
2022-01-27 21:05:17 +0100

    Expand import and export of process variables and experiments
    (only accessible with admin)

--- rtp_backend/apps/file_export_import/views.py
+++ rtp_backend/apps/file_export_import/views.py
@@ -19,6 +19,7 @@ from rtp_backend.apps.auth.helper_functions import is_admin
 from rtp_backend.apps.auth.models import User, UserTypeEnum
 from rtp_backend.apps.experiments.helper_functions import (
     get_experiment_short_id_from_pv_string,
+    pv_string_to_experiment,
     return_experiment_if_user_in_experiment,
 )
 from rtp_backend.apps.experiments.models import Experiment, ProcessVariable, db
@@ -37,6 +38,8 @@ from sqlalchemy import exc
 
 file_blueprint = Blueprint("file", __name__)
 
+SEMICOLON_PLACEHOLDER = "&SMKL&"
+
 
 def dict_to_csv(input_dict: dict, ignore_lists: bool = True):
     output_string = ""
@@ -47,13 +50,15 @@ def dict_to_csv(input_dict: dict, ignore_lists: bool = True):
         else:
             if output_string != "":
                 output_string += ";"
-            output_string += f"{key}={value}".replace(";", "\;")
+            output_string += f"{key}={value}".replace(";", SEMICOLON_PLACEHOLDER)
     return output_string
 
 
 @file_blueprint.route("/export", methods=["GET"])
 @token_required
 def export_file(current_user):
+    if current_user.user_type != UserTypeEnum.admin:
+        return forbidden_because_not_an_admin()
 
     lines = [
         "################################################",
@@ -95,7 +100,107 @@ def export_file(current_user):
     )
 
 
+def split_csv(line):
+    return [
+        attribute.replace(SEMICOLON_PLACEHOLDER, ";") for attribute in line.split(";")
+    ]
+
+
+def create_pv_from_csv(line, experiment_human_readable_name):
+    attributes = split_csv(line)
+
+    attributes_dict = {
+        "human_readable_name": None,
+        "default_threshold_max": None,
+        "default_threshold_min": None,
+    }
+    for attribute in attributes:
+        if attribute.startswith("pv_string="):
+            attribute.replace("pv_string=", "")
+            attributes_dict["pv_string"] = attribute
+
+        elif attribute.startswith("human_readable_name="):
+            attribute.replace("human_readable_name=", "")
+            attributes_dict["human_readable_name"] = attribute
+
+        elif attribute.startswith("experiment_short_id="):
+            attribute.replace("experiment_short_id=", "")
+            attributes_dict["experiment_short_id"] = attribute
+
+        elif attribute.startswith("available_for_mqtt_publish="):
+            attribute.replace("available_for_mqtt_publish=", "")
+            attributes_dict["available_for_mqtt_publish"] = bool(attribute)
+
+        elif attribute.startswith("default_threshold_min="):
+            attribute.replace("default_threshold_min=", "")
+            try:
+                attributes_dict["default_threshold_min"] = int(attribute)
+            except ValueError:
+                pass
+
+        elif attribute.startswith("default_threshold_max="):
+            attribute.replace("default_threshold_max=", "")
+            try:
+                attributes_dict["default_threshold_max"] = int(attribute)
+            except ValueError:
+                pass
+
+    pv = ProcessVariable.query.filter_by(pv_string=attributes_dict["pv_string"]).first()
+    if not pv:
+
+        pv = ProcessVariable(
+            pv_string=attributes_dict["pv_string"],
+            experiment_short_id=attributes_dict["experiment_short_id"],
+            human_readable_name=attributes_dict["human_readable_name"],
+            default_threshold_max=attributes_dict["default_threshold_max"],
+            default_threshold_min=attributes_dict["default_threshold_min"],
+        )
+
+        db.session.add(pv)
+    else:
+        pv.pv_string = attributes_dict["pv_string"]
+        pv.experiment_short_id = attributes_dict["experiment_short_id"]
+        pv.human_readable_name = attributes_dict["human_readable_name"]
+        pv.default_threshold_max = attributes_dict["default_threshold_max"]
+        pv.default_threshold_min = attributes_dict["default_threshold_min"]
+
+    experiment = pv_string_to_experiment(pv.pv_string)
+    if (
+        experiment_human_readable_name
+        and experiment
+        and not experiment.human_readable_name
+    ):
+        experiment.human_readable_name = experiment_human_readable_name
+
+    db.session.commit()
+
+
 @file_blueprint.route("/import", methods=["GET"])
 @token_required
 def import_file(current_user):
-    return ""
+    if current_user.user_type != UserTypeEnum.admin:
+        return forbidden_because_not_an_admin()
+
+    data = request.data
+    if not data:
+        return make_response({"errors": ["No data"]}, status.BAD_REQUEST)
+
+    data = data.decode("utf-8")
+    lines = data.split("\n")
+
+    last_experiment_human_readable_name = None
+
+    for line in lines:
+        line = line.strip()
+        line = bleach.clean(line)
+
+        if line.startswith("[EXPERIMENT]"):
+            attributes = split_csv(line)
+            for attribute in attributes:
+                if attribute.startswith("human_readable_name"):
+                    attribute.replace("human_readable_name=", "")
+                    last_experiment_human_readable_name = attribute
+        elif line.startswith("[PROCESS_VARIABLE]"):
+            create_pv_from_csv(line, last_experiment_human_readable_name)
+
+    return {"messages": ["Successful"]}
